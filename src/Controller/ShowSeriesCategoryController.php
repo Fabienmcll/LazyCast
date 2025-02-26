b90e629bc46069af0328338ee8f525036e9d1d3a
KilianBrd <kilianberaud3@gmail.com>
2025-02-26 21:42:52 +0100

    - Ajout de la search bar dans les séries

--- src/Controller/ShowSeriesCategoryController.php
+++ src/Controller/ShowSeriesCategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Service\XtreamApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +19,7 @@ class ShowSeriesCategoryController extends AbstractController
     }
 
     #[Route('/series/category/{category_id}', name: 'app_show_series_category')]
-    
+
     public function index(int $category_id): Response
     {
         session_start();
@@ -28,27 +29,56 @@ class ShowSeriesCategoryController extends AbstractController
         $this->vodApiService->setAction('get_series');
         $series = $this->getSeriesByCategory($category_id);
 
-        $toSend = $series->getContent();
+        $toSend = $this->json($series);
+
+        $toSend = $toSend->getContent();
         $toSend = json_decode($toSend, true);
 
         return $this->render('show_series_category/index.html.twig', [
             'controller_name' => 'ShowSeriesCategoryController',
             'series' => $toSend,
+            'category_id' => $category_id,
         ]);
     }
 
-    public function getSeriesByCategory(int $category_id): JsonResponse
+    public function getSeriesByCategory(int $category_id): array
     {
 
-        if($category_id == 1){
+        if ($category_id == 1) {
             $this->vodApiService->setAction('get_series');
             $series = $this->vodApiService->getAllSeries();
-            return $this->json($series);
+            return $series;
         } else {
             $series = $this->vodApiService->getWithCategoryId($category_id);
-            return $this->json($series);
+            return $series;
+        }
+    }
+
+    #[Route('/series/{category_id}/search', name: 'app_search_series', methods: ['GET'])]
+    public function searchSeries(Request $request, int $category_id): Response
+    {
+        $query = $request->query->get('q', '');
+        if (empty($query)) {
+            return $this->redirectToRoute('app_show_series_category', ['category_id' => 1]);
         }
 
-        
+        session_start();
+
+        $this->vodApiService->setUsername($_SESSION['username']);
+        $this->vodApiService->setPassword($_SESSION['password']);
+        $this->vodApiService->setApiUrl($_SESSION['server_url']);
+        $this->vodApiService->setAction('get_series');
+        $series = $this->getSeriesByCategory($category_id);
+
+        // Filtrer les résultats en fonction de la recherche (nom, genre, casting, etc.)
+        $filteredSeries = array_filter($series, function ($serie) use ($query) {
+            return stripos($serie['name'], $query) !== false;
+        });
+
+        return $this->render('show_series_category/index.html.twig', [
+            'controller_name' => 'ShowSeriesCategoryController',
+            'series' => $filteredSeries,
+            'category_id' => $category_id,
+        ]);
     }
 }
