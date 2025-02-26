0e9a4f4f62cd7d58faf1391fdd6d407986f3fbd9
Kilian Beraud <kilianberaud3@gmail.com>
2025-02-26 08:39:32 +0100

    Ajout du player

---
+++ src/Controller/IPTVSeriesControlelrController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IPTVSeriesControlelrController extends AbstractController
+{
+    #[Route('/iptvseries', name: 'app_i_p_t_v_series_controlelr')]
+    public function index(): Response
+    {
+        return $this->render('iptv_series_controlelr/index.html.twig', [
+            'controller_name' => 'IPTVSeriesControlelrController',
+        ]);
+    }
+}
