7220aa05493974074386f109aa5c0143e949f849
Fabienmcll <fabienmclll@gmail.com>
2025-02-26 10:48:06 +0100

    - Installation de TailwindCSS V4.

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
-import './style/style.scss';
+    import './bootstrap.js';
+    import './styles/app.css';
 
-// eslint-disable-next-line no-console
-console.log('Hello from app.js');
+    // eslint-disable-next-line no-console
+    console.log('Hello from app.js');
